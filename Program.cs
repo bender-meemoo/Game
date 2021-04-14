63ae90e3d2fbf99504b98e7fefd645b642b1ac54
Eric H. Smith <esmith167@gmail.com>
2021-04-13 22:48:47 -0400

    Added stuff

--- Program.cs
+++ Program.cs
@@ -7,12 +7,12 @@ namespace Game
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine("Hello World!");
-            // Human eric = new Human("Eric,",20,20,20,20);
-            // Human mitch = new Human("Mitch");
-            // Console.WriteLine(mitch.Health);
-            // eric.Attack(mitch);
-            // Console.WriteLine(mitch.Health);
+            Console.WriteLine("Hello World!");
+            Human eric = new Human("Eric,",20,20,20,20);
+            Human mitch = new Human("Mitch");
+            Console.WriteLine(mitch.Health);
+            eric.Attack(mitch);
+            Console.WriteLine(mitch.Health);
 
             
         }
