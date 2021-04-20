68fdb5c00a11e679d8a8f8f13adc426c84cb1866
Eric H. Smith <esmith167@gmail.com>
2021-04-20 17:52:43 -0400

    added attacks and heals

--- models/Human.cs
+++ models/Human.cs
@@ -1,54 +1,112 @@
+using System;
+
 namespace Game.models
 {
     public class Human
     {
 
 
-    public string Name;
-    public int Strength;
-    public int Intelligence;
-    public int Dexterity;
-    private int health;
-    public int Health
-    {
-        get
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        protected int health;
+        public int Health
         {
-            return health;
+            get { return health; }
         }
-    }
-    public Human(string charname)
-    {
-        Name = charname;
-        Strength = 3;
-        Intelligence = 3;
-        Dexterity = 3;
-        health = 100;
-    }
+        public Human(string charname)
+        {
+            Name = charname;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            health = 100;
+        }
 
-     public Human(string charname, int str, int intel, int dex, int hp)
-    {
-        Name = charname;
-        Strength = str;
-        Intelligence = intel;
-        Dexterity = dex;
-        health = hp;
-    }
-    
+        public Human(string charname, int str, int intel, int dex, int hp)
+        {
+            Name = charname;
+            Strength = str;
+            Intelligence = intel;
+            Dexterity = dex;
+            health = hp;
+        }
 
-        // *add a public "getter" property to access health
 
-        // Add a constructor that takes a value to set Name, and set the remaining fields to default values
-
-        // Add a constructor to assign custom values to all fields
-
-        // Build Attack method
-        public int Attack(Human target)
+        public virtual int Attack(Human target)
         {
             target.health -= Strength * 5;
             return target.health;
         }
+        public int TakeDamage(int damage)
+        {
+            health -= damage;
+            return health;
+        }
     }
 
+    public class Wizard : Human
+    {
+        public Wizard(string wiz) : base(wiz)
+        {
+            health = 50;
+            Intelligence = 25;
+        }
+        public int heal(Human target)
+        {
+            int heal = Intelligence * 5;
+            target.TakeDamage(heal * -1);
+            return target.Health;
 
+        }
+    }
+    public class Ninja : Human
+    {
+        public Random rand = new Random();
+        public Ninja(string name) : base(name)
+        {
+            Dexterity = 175;
+        }
+        public override int Attack(Human target)
+        {
+            int dmg = Dexterity * 5;
+            int TwentyPercentChance = rand.Next(1, 11);
+            if (TwentyPercentChance <= 2)
+            {
+                dmg += 10;
+                Console.WriteLine("Booyah!");
+            }
+            target.TakeDamage(dmg);
+            Console.WriteLine($"{Name} attacked {target.Name} for {dmg} damage!");
+            if (target.Health <= 0)
+            {
+                Console.WriteLine($"{Name} has defeated {target.Name}");
+            }
+            return target.Health;
+        }
+        public int Steal(Human target)
+        {
+            int dmg = 5;
+            target.TakeDamage(dmg);
+            TakeDamage(-dmg);
+            Console.WriteLine($"{Name} has stolen {dmg} health from {target.Name}");
+            return target.Health;
+        }
+    }
+
+    public class Samurai : Human
+    {
+        public Samurai(string sam) : base (sam)
+        {
+            health = 200;
+        }
+    }
+        public int meditate(Human target)
+            {
+                target.TakeDamage(meditate = 200);
+                return target.Health;
+
+            }
 
 }
